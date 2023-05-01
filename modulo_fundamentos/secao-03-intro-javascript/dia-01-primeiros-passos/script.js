3e57b0e31457326aab1fa97b566bd7629842eb14
Gabriel Muniz <gabrielmfd@gmail.com>
2023-05-01 20:48:26 -0300

    finalização exercicio JS Tipos primitivos e tipagem dinamica

--- modulo_fundamentos/secao-03-intro-javascript/dia-01-primeiros-passos/script.js
+++ modulo_fundamentos/secao-03-intro-javascript/dia-01-primeiros-passos/script.js
@@ -1,4 +1,25 @@
+// Exercicio numero 1 Variaveis:
+
 let myName = 'Gabriel Muniz';
 let birthCity = 'Niterói';
 let birthYear = 1988;
-console.log(myName, birthCity, birthYear);
+//console.log(myName, birthCity, birthYear);
+
+// Exercicio numero 2 Tipos primitivos, tipagem dinamica: // 
+
+let patientId = 50;
+let isEnrolled = true;
+let patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+let patientEmail = 'ana@email.com';
+
+//console.log(typeof patientId);
+
+//Exercicio 3 para fixar Operadores :
+let base = 5;
+let height = 8;
+let area = base * height;
+let perimeter = 2*(base + height);
+console.log(perimeter);
