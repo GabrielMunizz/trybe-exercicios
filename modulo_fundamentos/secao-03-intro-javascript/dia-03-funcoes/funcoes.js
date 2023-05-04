202adce360d5ea1efd7cb4f64b5b3a147d9a36d1
Gabriel Muniz <gabrielmfd@gmail.com>
2023-05-04 10:18:14 -0300

    Resolução do primeiro exercicio de funções

---
+++ modulo_fundamentos/secao-03-intro-javascript/dia-03-funcoes/funcoes.js
@@ -0,0 +1,20 @@
+let saldoInicial = 100;
+let saldoFinal;
+function adiciona(dinheiros) {
+    return saldoFinal = saldoInicial + dinheiros
+    
+}
+function subtrai(dinheiros) {
+    return saldoFinal = saldoInicial - dinheiros
+}
+function multiplica(dinheiros) {
+    return saldoFinal = saldoInicial * dinheiros
+}
+function divide(dinheiros) {
+    return saldoFinal = saldoInicial / dinheiros
+}
+
+console.log(adiciona(299));
+console.log(subtrai(50));
+console.log(multiplica(5));
+console.log(divide(3));
