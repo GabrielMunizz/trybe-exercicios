a1586ce961803f66f854cc89773e0e610139080d
Gabriel Muniz <gabrielmfd@gmail.com>
2023-05-04 21:47:02 -0300

    finalização de outro exercicio

--- modulo_fundamentos/secao-03-intro-javascript/dia-03-funcoes/funcoes.js
+++ modulo_fundamentos/secao-03-intro-javascript/dia-03-funcoes/funcoes.js
@@ -1,4 +1,5 @@
-let saldoInicial = 100;
+//Exercicio 1 Funções
+/*let saldoInicial = 100;
 let saldoFinal;
 function adiciona(dinheiros) {
     return saldoFinal = saldoInicial + dinheiros
@@ -18,3 +19,32 @@ console.log(adiciona(299));
 console.log(subtrai(50));
 console.log(multiplica(5));
 console.log(divide(3));
+*/
+
+//Exercicio 2 Funções
+
+/*let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+
+function addClient(cliente) {
+    if (typeof cliente === 'string') {
+        clientesTrybeBank.push(cliente);
+        return clientesTrybeBank;
+    }else {
+        return 'Erro';
+    }
+}
+function deleteClient(cliente) {
+    if (typeof cliente === 'string') {
+        for (let i = 0; i < clientesTrybeBank.length; i += 1) {
+            if (cliente === clientesTrybeBank[i]) {
+                clientesTrybeBank.splice(i, 1);
+                return 'Cliente excluido com sucesso'
+            }
+        }        
+    }else {
+        return "Erro. O cliente não é uma string"
+    }
+}
+console.log(deleteClient('John'));
+// escreva aqui sua função */
+
