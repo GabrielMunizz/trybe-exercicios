87ec3eebaed855e1a100073f47dfe3e01680dc7f
Gabriel Muniz <gabrielmfd@gmail.com>
2023-05-07 14:47:47 -0300

    Finalização exercicio de objetos

--- modulo_fundamentos/secao-03-intro-javascript/dia-04-objetos/script.js
+++ modulo_fundamentos/secao-03-intro-javascript/dia-04-objetos/script.js
@@ -6,4 +6,9 @@ let player = {
         medals: { golden: 2, silver: 3 }        
     }
 
-console.log(player.medals.golden)
+console.log('A jogadora '+ player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.')
+
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
+player.fullName = player.name + " " + player.lastName
+console.log('A jogadora ' + player['fullName'] + " foi eleita a melhor do mundo por " + player['bestInTheWorld'].length + " vezes");
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata')
