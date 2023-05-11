41ff297489bca8f73a0acb53b518d76b838b4889
Gabriel Muniz <gabrielmfd@gmail.com>
2023-05-11 13:46:26 -0300

    Resolução de exercicios referente a aula do dia

---
+++ modulo_fundamentos/secao-04-javascript-dom-eventos-web_storage/dia-01-DOM-seletores/script2.js
@@ -0,0 +1,6 @@
+let paragraphs = document.getElementsByClassName('paragraphs');
+paragraphs[0].style.color = 'red';
+paragraphs[1].style.backgroundColor = 'yellow';
+let subtitle = document.getElementsByTagName('h4');
+subtitle[0].style.backgroundColor = 'blue';
+subtitle[0].innerText = 'Gabriel'
