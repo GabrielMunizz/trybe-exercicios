a2b76be4ce0a79c9da5941778b4c3830fa088777
Gabriel Muniz <gabrielmfd@gmail.com>
2023-05-11 12:10:06 -0300

    Criação branch exercicios/fundamentos-4.1 e arquivos referente aos estudos do dia

---
+++ modulo_fundamentos/secao-04-javascript-dom-eventos-web_storage/dia-01-DOM-seletores/script.js
@@ -0,0 +1,7 @@
+document.getElementById("page-title").innerText = 'Shawshank Redemption';
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+paragraph.style.backgroundColor = "black";
+const subtitle = document.getElementById("subtitle");
+subtitle.style.color = 'green';
+subtitle.style.backgroundColor = 'yellow'
