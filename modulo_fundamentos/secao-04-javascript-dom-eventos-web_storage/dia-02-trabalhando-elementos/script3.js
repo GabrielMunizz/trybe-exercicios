25aeffa1199079c5ed1bcc055452a53b2a409b5f
Gabriel Muniz <gabrielmfd@gmail.com>
2023-05-15 13:49:47 -0300

    atualização de arquivos

--- modulo_fundamentos/secao-04-javascript-dom-eventos-web_storage/dia-02-trabalhando-elementos/script3.js
+++ modulo_fundamentos/secao-04-javascript-dom-eventos-web_storage/dia-02-trabalhando-elementos/script3.js
@@ -1,3 +1,6 @@
 let element = document.querySelector("#where-are-you");
 let filhoElement = document.querySelector('#where-are-you h2')
-element.removeChild(filhoElement)
+element.removeChild(filhoElement);
+let arya = document.getElementById('stark_childs').lastElementChild.innerHTML
+console.log(arya)
+
